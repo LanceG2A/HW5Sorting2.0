054b2dc474eb346eab514bfd7d8834e5ab748314
LGA2 <lackerson@albany.edu>
2017-04-23 13:16:30 -0400

    link and list code

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,4 +1,91 @@
 
 public class DoublyLinkedList {
 
+	private Link head; //field to store the first Link
+	private int size;  //field to hold the size of the list
+	
+	public DoublyLinkedList(){  //Base constructor to create an empty list
+		head = null;
+		size = 0;
+	}
+	
+	public void addStart(int randomNumber)//to add a link in the list at the beginning
+	{
+		if(head == null)		//checks if the list is empty
+		{						//if it is empty it sets the first Link to "color"
+			head = new Link(null, randomNumber, null); //and since it is the only Link, 
+		}										// next and previous are null (empty)
+		else
+		{
+			Link newLink = new Link(null, randomNumber, head);// makes the "next" link the head node
+			head.previous = newLink;					//takes old head (start) and sets previous
+			head = newLink;								//to the new start we just made
+		}
+		size++;
+	}	
+	public int size()
+	{
+		return size;
+	}
+	public void printListForward(){//iterates through the list and prints each Link
+		Link current = head;
+		if(current != null){
+			while(current != null){
+				System.out.println(current.randomNumber);
+				current = current.next;
+		}
+		}
+		else{
+			System.out.println("Empty List");
+		}
+	}
+	public void printListBackward(){//iterates forward through the list, then prints from back to front
+		Link current = head;
+		if(current != null){
+		while(current.next != null){
+			current = current.next;
+		}
+		System.out.println(current.randomNumber);
+		while(current.previous != null){
+			System.out.println(current.previous.randomNumber);
+			current = current.previous;
+		}
+		}
+		else{
+			System.out.println("Empty List");
+		}
+	}
+	
+	public boolean isEmpty(){
+		return head == null;	//returns true if head is null
+	}
+	
+
+	
+	public void deleteLink(int randomNumber){ //deletes a node base on the color name it contains
+		Link temp = head;
+		while(temp.randomNumber != randomNumber){
+			temp = temp.next;
+		}
+		Link next = temp.next;
+		Link previous = temp.previous;
+		next.previous = previous;
+		previous.next = next;
+	}
+
+		
+		
+	
+	public void destroyList(){
+		Link temp = head;
+		while(temp != null){
+			head = null;
+			Link temp2 = temp;
+			temp = temp.next;
+			temp2.previous = null;
+			temp2.next = null;
+			temp2 = null;
+		}
+	}
 }
+	
--- Link.java
+++ Link.java
@@ -1,4 +1,24 @@
 
-public class Link {
+public class Link { //Class to for Links (Nodes)
 
+	int randomNumber;	//Fields for data,
+	public Link next;	//and previous/next links(nodes)
+	public Link previous;
+	
+	public Link(int randomNumber) //Constructor initialializes link
+	{							//
+		previous = null;
+		this.randomNumber = randomNumber;
+		next = null;
+	}
+	public Link(Link previous, int randomNumber, Link next) { 
+		this.previous = previous;  //
+		this.randomNumber = randomNumber;
+		this.next = next;	
+	}
+	
+	public String ReturnNumber() {  //Return link information
+		return this.randomNumber;
+	}
+	
 }
