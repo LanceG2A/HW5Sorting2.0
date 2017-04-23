59511f2f7d07483f3d419c4e194fdb895ebfbd8f
LGA2 <lackerson@albany.edu>
2017-04-23 13:21:54 -0400

    minor adjustments

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,4 +1,16 @@
+import java.util.Random;
 
 public class BubbleSort {
+ Random rand = new Random();
+ DoublyLinkedList numbers = new DoublyLinkedList();
+ int[] randomArray = new int[];
+ 
+ public BubbleSort(int[] array){
+	 
+ }
+ 
+}
 
+public static void main(String[] args){
+	System.out.
 }
